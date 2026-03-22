5b06ed6b5aedf23088b26dc42cfefb0585e5da02
dipbhi <dipen.bhikadya@gmail.com>
2026-03-22 15:13:47 +0530

    first commit

---
+++ nuxt.config.ts
@@ -0,0 +1,11 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  modules: [
+    '@nuxt/content',
+  ],
+  devtools: { enabled: true },
+  compatibilityDate: '2024-04-03',
+  content: {
+    experimental: { sqliteConnector: 'better-sqlite3' },
+  }
+})
